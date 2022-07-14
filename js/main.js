f420d469ba2b02ba9d7230e18127e6fba0a64d1d
Andrea Quaglioni <quaglioniandrea@gmail.com>
2022-07-14 16:08:34 +0200

    add: uniqueRandomNumber function to create the array containing the position of "bombs"

--- js/main.js
+++ js/main.js
@@ -10,27 +10,68 @@ playButton.addEventListener(
     const difficultyN = parseInt(difficulty);
     console.log("difficultyN: ", typeof difficultyN);
 
-    createToggableProgressiveNumberedBox(difficultyN);
-  },
-//  { once: true } non più necessario perché containerDom.innerHTML = "" resetta il contenuto
+    createToggableProgressiveNumberedBoxAndBombs(difficultyN);
+  }
+  //  { once: true } non più necessario perché containerDom.innerHTML = "" resetta il contenuto
 );
 
-function createToggableProgressiveNumberedBox(number) {
+function createToggableProgressiveNumberedBoxAndBombs(number) {
+  const bombNumbers = [];
+  console.log("bombNumbers: ", bombNumbers);
+
+  for (let i = 0; i < 16; i++) {
+    const randomNumber = uniqueRandomNumber(bombNumbers, 1, number);
+    bombNumbers.push(randomNumber);
+  }
+
   for (let i = 1; i <= number; i++) {
     const addBox = document.createElement("div");
     addBox.className = "box";
-    if (difficulty.value == '81') {
-        addBox.classList.add("box9");
-    } else if (difficulty.value == '49') {
-        addBox.classList.add("box7");
-    } else if (difficulty.value == '100') {
-        addBox.classList.add("box10");
+    if (difficulty.value == "81") {
+      addBox.classList.add("box9");
+    } else if (difficulty.value == "49") {
+      addBox.classList.add("box7");
+    } else if (difficulty.value == "100") {
+      addBox.classList.add("box10");
     }
     containerDom.append(addBox);
     addBox.append(i);
+
     addBox.addEventListener("click", function () {
-      addBox.classList.toggle("active");
-      console.log(i)
+      if (bombNumbers.includes(i)) {
+        addBox.classList.toggle("bomb");
+      } else {
+        addBox.classList.toggle("active");
+        console.log(i);
+      }
     });
   }
 }
+
+function uniqueRandomNumber(array, min, max) {
+  let validNumber = false;
+  let generatedNumber;
+  while (!validNumber) {
+    generatedNumber = getRandomNumber(min, max);
+    if (!array.includes(generatedNumber)) {
+      validNumber = true;
+    }
+  }
+  return generatedNumber;
+}
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+/*
+function getArrayOfRandomUniqueNumbers (array){
+    let randomNumber;
+    for (let i = 0; i < 16; i++) {
+      randomNumber = getRandomNumber(1, 100);  //////da cambiare
+      if (!array.includes(randomNumber)) {
+        array.push(randomNumber);   
+      }
+    }
+    return randomNumber;
+}
+*/
